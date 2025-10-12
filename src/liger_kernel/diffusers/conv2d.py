367d5fe7159914db238d8e8ad8c5915c93b017a6
Martin Nguyen <nguyenquangduc2000@gmail.com>
2025-10-12 22:35:51 +0800

    Update Layer implementation

--- src/liger_kernel/diffusers/conv2d.py
+++ src/liger_kernel/diffusers/conv2d.py
@@ -2,16 +2,32 @@ import torch
 import torch.nn as nn
 from liger_kernel.ops.conv2d import TritonConv2dFunction
 
-class TritonConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0):
+
+class LigerConv2d(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        padding=0,
+        device=None,
+        dtype=None,
+    ):
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
-        self.weight = nn.Parameter(torch.randn(out_channels, in_channels, *kernel_size))
-        self.bias = nn.Parameter(torch.randn(out_channels))
+        self.weight = nn.Parameter(
+            torch.randn(
+                out_channels, in_channels, *kernel_size, device=device, dtype=dtype
+            )
+        )
+        self.bias = nn.Parameter(torch.randn(out_channels, device=device, dtype=dtype))
 
     def forward(self, x):
-        return TritonConv2dFunction.apply(x, self.weight, self.bias, self.padding, self.stride)
+        return TritonConv2dFunction.apply(
+            x, self.weight, self.bias, self.padding, self.stride
+        )
--- src/liger_kernel/diffusers/linear.py
+++ src/liger_kernel/diffusers/linear.py
@@ -4,11 +4,22 @@ from liger_kernel.ops.linear import LigerLinearFunction
 
 
 class LigerLinear(torch.nn.Module):
-    r"""Applies a linear transformation to the incoming data: :math:`y = xA^T + b`
-    """
+    r"""Applies a linear transformation to the incoming data: :math:`y = xA^T + b`"""
 
-    def __init__(self):
+    def __init__(self, in_features, out_features, bias=True, device=None, dtype=None):
         super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.bias = bias
+        self.device = device
+        self.dtype = dtype
+        self.weight = torch.nn.Parameter(
+            torch.empty((out_features, in_features), device=device, dtype=dtype)
+        )
+        if bias:
+            self.bias = torch.nn.Parameter(
+                torch.empty((out_features,), device=device, dtype=dtype)
+            )
 
-    def forward(self, x: torch.Tensor, weight: torch.Tensor, bias: Optional[torch.Tensor]):
-        return LigerLinearFunction.apply(x, weight, bias)
+    def forward(self, x: torch.Tensor):
+        return LigerLinearFunction.apply(x, self.weight, self.bias)
